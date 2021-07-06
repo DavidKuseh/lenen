01855598ff730045af9d27477ad13dd5b039b253
david kuseh <davegru203@gmail.com>
2021-07-06 13:03:46 +0000

    set up server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get( "/", (req, res, next) => {
+    res.send("<p>Landing page</p>")
+})
+
+const port = 4000;
+
+app.listen({port}, () => {
+    console.log(`server up and running on http://localhost:${port}`)
+})
