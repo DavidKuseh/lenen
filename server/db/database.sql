03c8bd3a1adfcbde8b829172e7238436ddce95bd
david kuseh <davegru203@gmail.com>
2021-07-08 21:44:18 +0000

    refactor user account table

--- server/db/database.sql
+++ server/db/database.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE book_rental;
 
 CREATE TABLE user_account(
-    user_id SERIAL PRIMARY KEY,
-    email citext UNIQUE,
-    password varchar(255),
-    role text
+  user_id SERIAL PRIMARY KEY,
+  user_email VARCHAR(255) NOT NULL UNIQUE,
+  user_password VARCHAR(255) NOT NULL,
+  user_role text NOT NULL
 );
