6dcecbc55665c1ba445c4f441efe8643f0d9a71b
david kuseh <davegru203@gmail.com>
2021-07-12 23:53:02 +0000

    create user roles table

--- server/db/database.sql
+++ server/db/database.sql
@@ -5,3 +5,11 @@ CREATE TABLE user_account(
   user_email VARCHAR(255) NOT NULL UNIQUE,
   user_password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE user_roles(
+  user_id INTEGER NOT NULL,
+  role_id INTEGER NOT NULL,
+  role_desc TEXT NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES user_account(user_id),
+  PRIMARY KEY(user_id, role_id)
+);
