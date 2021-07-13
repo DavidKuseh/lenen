c2c2afa02f7d45e1ba9551fdfefacefc9603fca7
david kuseh <davegru203@gmail.com>
2021-07-13 16:54:40 +0000

    update db tables

--- server/db/database.sql
+++ server/db/database.sql
@@ -3,13 +3,11 @@ CREATE DATABASE book_rental;
 CREATE TABLE user_account(
   user_id SERIAL PRIMARY KEY,
   user_email VARCHAR(255) NOT NULL UNIQUE,
-  user_password VARCHAR(255) NOT NULL
+  user_password VARCHAR(255) NOT NULL,
+  user_role VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE user_roles(
-  user_id INTEGER NOT NULL,
-  role_id INTEGER NOT NULL,
-  role_desc TEXT NOT NULL,
-  FOREIGN KEY(user_id) REFERENCES user_account(user_id),
-  PRIMARY KEY(user_id, role_id)
+CREATE TABLE roles(
+  id INTEGER NOT NULL PRIMARY KEY,
+  user_role VARCHAR(20) NOT NULL 
 );
