b86d1d4bf35be9eb84e20bf5228a84e51b5f97c7
david kuseh <davegru203@gmail.com>
2021-07-14 20:58:25 +0000

    make changes to db

--- server/db/database.sql
+++ server/db/database.sql
@@ -1,13 +1,8 @@
 CREATE DATABASE book_rental;
 
 CREATE TABLE user_account(
-  user_id SERIAL PRIMARY KEY,
-  user_email VARCHAR(255) NOT NULL UNIQUE,
-  user_password VARCHAR(255) NOT NULL,
-  user_role VARCHAR(20) NOT NULL
-);
-
-CREATE TABLE roles(
-  id INTEGER NOT NULL PRIMARY KEY,
-  user_role VARCHAR(20) NOT NULL 
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  role text NOT NULL
 );
