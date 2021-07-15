ec383368e476d1df684d6d8ce18018a554374acb
david kuseh <davegru203@gmail.com>
2021-07-15 11:46:22 +0000

    knex install

--- server/db/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE book_rental;
-
-CREATE TABLE user_account(
-  id SERIAL PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL,
-  role text NOT NULL
-);
