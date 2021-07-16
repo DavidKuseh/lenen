ec86b9c70aa2db17324b09de3779738ab1cbcb1d
david kuseh <davegru203@gmail.com>
2021-07-16 11:07:48 +0000

    create migrations

---
+++ server/db/migrations/20210716093805_init.js
@@ -0,0 +1,34 @@
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('roles', table => {
+            table
+                .increments();
+            table
+                .string('name', 128)
+                .notNullable()
+                .unique();
+        })
+        .createTable('users', table => {
+            table
+                .increments();
+            table
+                .string('email', 128)
+                .notNullable()
+                .unique();
+            table   
+                .string('password', 128)
+                .notNullable()
+            table
+                .integer('role')
+                .unsigned()
+                .references('users.id')
+                .onUpdate('CASCADE')
+                .defaultTo(2)
+      });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('users')
+    .dropTableIfExists('roles');
+};
---
+++ server/knexfile.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: process.env.CLIENT,
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    },
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+}
+
