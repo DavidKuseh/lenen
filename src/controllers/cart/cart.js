a424382b15eddb7a32acda61910fc729e7921267
David Kuseh <davegru203@gmail.com>
2021-10-19 17:37:02 +0000

    create add to cart and get cart controller functions

--- src/controllers/cart/cart.js
+++ src/controllers/cart/cart.js
@@ -1,7 +1,40 @@
 import * as Cart from '../../models/cart/cart.js';
+import * as Book from '../../models/books/books.js';
 
-export const addBook = (req, res) => {
+export const addBookToCart = async (req, res) => {
+    try {
+        const _book_id = req.params.book_id;
+
+        const { subject } = req.decoded;
+
+        const cartExists = await Cart.getCartById(subject);
+
+        if(cartExists == null || cartExists.length == 0 || cartExists == []) {
+            const bookDetails = await Book.getBookById(_book_id);
+            const cost = bookDetails.price;
+
+            var booksArray = [];
+            booksArray.push(_book_id);
+
+            await Cart.addToCart({
+                user_id: req.decoded.subject, 
+                book_ids: booksArray,
+                cost,
+                number_of_items: booksArray.length
+            });
+            res.redirect('/api/cart');
+        } else {
+            res.redirect('/')
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    };
 };
 
 export const getCartPage = async (req, res) => {
+    try {
+        res.render('cart', { title: "Cart"});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    };
 };
