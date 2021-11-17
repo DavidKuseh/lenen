8e1c404d64c197151595c6fba4a430f6622188f7
David Kuseh <davegru203@gmail.com>
2021-11-17 08:29:15 +0000

    fix errors

--- src/db/seeds/02-users.js
+++ src/db/seeds/02-users.js
@@ -1,16 +1,19 @@
-export function user_seed(knex) {
+export function seed(knex) {
     const users = [
         {
+            username: "admin1",
             email: 'admin1@gmail.com',
             password: 'bigSecret123',
             role: 1
         },
         {
+            username: "user1",
             email: 'user1@gmail.com',
             password: 'smallSecret123',
             role: 2
         },
         {
+            username: "user2",
             email: 'user2@gmail.com',
             password: 'notsobigSecret123',
             role: 2
@@ -18,6 +21,6 @@ export function user_seed(knex) {
     ]
 
     return knex('users')
-        , insert(users)
-            .then(() => console.log('\n== seed data for roles table added ==\n'));
+        .insert(users)
+            .then(() => console.log('\n== seed data for users table added ==\n'));
 }
